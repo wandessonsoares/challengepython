aa8d229a566fa0fa78d4d9bbe6fc2116a0494e7a
Wandesson Soares <wandessonsilvatsi@gmail.com>
2019-05-22 23:51:18 -0300

    salvando conteudo da requisição em arquivo json

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import requests
+import json
 
 url = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token='
 token = '62ebf2cee933356a5e46eb480772725b55b3af4f'
 
-response = requests.get(url + token);
+response = requests.get(url + token)
 
-print(response.content);
+conteudo = response.content
+conteudo_decodificado = json.loads(conteudo) 
+
+def salvar_json(conteudo):
+  conteudo_json = json.dumps(conteudo)
+  arquivo = open('answer.json', 'a')
+  arquivo.write(conteudo_json)
+  arquivo.close()
+
+salvar_json(conteudo_decodificado)
